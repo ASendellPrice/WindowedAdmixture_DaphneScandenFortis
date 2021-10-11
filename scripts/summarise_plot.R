e780fae9256efa4a7074a7b507469afc0282eda2
ASendellPrice <ashley.sendell-price@zoo.ox.ac.uk>
2021-10-11 11:58:20 +0200

    sc

--- scripts/summarise_plot.R
+++ scripts/summarise_plot.R
@@ -11,11 +11,8 @@ Q.files <- list.files(pattern = ".Q")
 xlist <- alignK(readQ(Q.files), type = "within")
 
 #Load window list
-windows <- read.delim("../../window.ranges.txt", header = FALSE, sep = " ")
+windows <- read.delim("../../window.ranges.txt", header = FALSE, sep = ":")
 colnames(windows) <- c("start","end","mid")
-
-#Remove windows with zero SNPs
-#windows <- filter(windows, sites > 0)
 
 #Load sample ID and species info
 sample_info <- read.delim("../../sample_info.tmp", header = FALSE)
@@ -31,7 +28,7 @@ combined <- right_join(sample_order, sample_info)
 #For each window do the following . . .
 for (WINDOW in 1:nrow(windows)){
 
-        x <- paste0("window.",windows$start[WINDOW],"-",windows$end[WINDOW],".AllSamples.2.Q")
+        x <- paste0("window.",windows$start[WINDOW],"-",windows$end[WINDOW],".2.Q")
         window.Q <- cbind(combined, as.data.frame(xlist[[x]][[1]]), as.data.frame(xlist[[x]][[2]]))
         colnames(window.Q) <- c("sampleID","location","species","cluster1","cluster2")
         window.Q$mid <- windows$mid[WINDOW]
